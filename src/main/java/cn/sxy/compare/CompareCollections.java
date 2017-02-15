2a1804e554b0f1c6f97b8851f55f8189e8e108a7
sxy <sxy@rebind.cn>
2017-02-15 20:08:23 +0800

    return HashMap rather than Map

--- src/main/java/cn/sxy/compare/CompareCollections.java
+++ src/main/java/cn/sxy/compare/CompareCollections.java
@@ -11,8 +11,8 @@ import static java.lang.Math.min;
 // PK是对象的主键， T是对象
 public class CompareCollections<PK, T> {
 
-    private Map<PK, T> before;
-    private Map<PK, T> after;
+    private HashMap<PK, T> before;
+    private HashMap<PK, T> after;
 
     private CompareCollections() {
     }
@@ -24,21 +24,23 @@ public class CompareCollections<PK, T> {
         return c;
     }
 
-    public static <T> CompareCollections<T, T> create(Collection<T> before, Collection<T> after) {
+    public static <T> CompareCollections<T, T> create(Collection<T> before, Collection<T> after)
+            throws IllegalStateException {
         CompareCollections<T, T> c = new CompareCollections<>();
-        c.before = before.stream().collect(Collectors.toMap(e -> e, e -> e));
-        c.after = after.stream().collect(Collectors.toMap(e -> e, e -> e));
+        c.before = (HashMap<T, T>) before.stream().collect(Collectors.toMap(e -> e, e -> e));
+        c.after = (HashMap<T, T>) after.stream().collect(Collectors.toMap(e -> e, e -> e));
         return c;
     }
 
-    public static <PK, T> CompareCollections<PK, T> create(Collection<T> before, Collection<T> after, Function<T, PK> getPK) {
+    public static <PK, T> CompareCollections<PK, T> create(Collection<T> before, Collection<T> after, Function<T, PK> getPK)
+            throws IllegalStateException {
         CompareCollections<PK, T> c = new CompareCollections<>();
-        c.before = before.stream().collect(Collectors.toMap(getPK, e -> e));
-        c.after = after.stream().collect(Collectors.toMap(getPK, e -> e));
+        c.before = (HashMap<PK, T>) before.stream().collect(Collectors.toMap(getPK, e -> e));
+        c.after = (HashMap<PK, T>) after.stream().collect(Collectors.toMap(getPK, e -> e));
         return c;
     }
 
-    public Map<PK, T> getAddMap() {
+    public HashMap<PK, T> getAddMap() {
         Map<PK, T> addMap = new HashMap<>(after.size());
         after.forEach((pk, t) -> {
             if (!before.containsKey(pk)) {
@@ -48,7 +50,7 @@ public class CompareCollections<PK, T> {
         return new HashMap<>(addMap);
     }
 
-    public Map<PK, T> getSubMap() {
+    public HashMap<PK, T> getSubMap() {
         Map<PK, T> subMap = new HashMap<>(before.size());
         before.forEach((pk, v) -> {
             if (!after.containsKey(pk)) {
@@ -59,7 +61,7 @@ public class CompareCollections<PK, T> {
     }
 
     // T[0] 是 before， T[1] 是 after
-    public Map<PK, T[]> getUpdateMap() {
+    public HashMap<PK, T[]> getUpdateMap() {
         Map<PK, T[]> updateMap = new HashMap<>(min(before.size(), after.size()));
         after.forEach((pk, afterV) -> {
             T beforeV = before.get(pk);
